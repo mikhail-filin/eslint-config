9e6104a1483b5139153a5dbe9e1dbc43a6cadfa5
Mikhail Filin <mikhailf.codes@gmail.com>
2023-11-10 21:51:45 +0400

    feat: replace eslint config with flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
-module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		es2021: true,
+/*
+   Sticking to CJS as Webstorm doesn't support flat configs with ES modules yet.
+   Refer to https://youtrack.jetbrains.com/issue/WEB-57661/
+*/
+module.exports = [
+	{
+		rules: {
+			semi: ['error', 'always'],
+		},
 	},
-	extends: 'eslint:recommended',
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
-	rules: {
-		semi: ['error', 'always'],
-	},
-};
+];
